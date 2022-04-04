242db1316433407dcd0180cd6c61efb6fe247ae2
armancse-dev <armanhossaincse7@gmail.com>
2022-04-04 22:18:24 +0600

    Dynamic Content, Dynamic Style In React

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const number = 555;
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hellow React Mamma!!</h1>
+        <p>Doira felse tomre koi jaba</p>
+        <div className="container">
+          <h3>This is inside my container</h3>
+          <p>My number is: {number}</p>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
