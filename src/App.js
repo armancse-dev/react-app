cbbb9876751276ff89545803ca63a9c72728e1ff
armancse-dev <armanhossaincse7@gmail.com>
2022-04-04 22:27:59 +0600

    Jsx

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import './App.css';
 
 function App() {
   const number = 555;
+  const singer ={
+    name: 'Dr. Mahfuz',
+    job: 'singer'
+  }
+  const singer2={
+    name: 'Eva Rahman',
+    job: 'Kokil Konthi Gaan ar pakhi'
+  }
+
+  const singerStyle = {
+    backgroundColor: 'red',
+    color: 'white'
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +24,11 @@ function App() {
         <div className="container">
           <h3>This is inside my container</h3>
           <p>My number is: {number}</p>
+          <p style={{
+            color: 'magenta',
+            fontSize: '34px'
+          }}>Singer: {singer.name + ' ' + singer.job}</p>
+          <p style= {singerStyle}>Real Singer: {singer2.job}</p>
         </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
