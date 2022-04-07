20106cb573fd592298c6036065c7e89e6823da7c
armancse-dev <armanhossaincse7@gmail.com>
2022-04-07 22:37:20 +0600

    Create Multiple Components From An Array Of Objects

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  
+  const nayoks = ['Rubel', 'BappaRaz', 'shovu', 'Omar Sani'];
+
+  const cinemas = [
+    { nayok: 'Koober', naika: 'Kopila'},
+    { nayok: 'Rubel', naika: 'Moushumi'},
+    { nayok: 'Razzak', naika: 'Shabana'},
+    { nayok: 'Jashim', naika: 'Suchorita'},
+  ]
   return (
     <div className="App">
-      <Person name="Rubel" naika="Moushomi"></Person>
-      <Person name="BappaRaz" naika="Cheka"></Person>
+      <ul>
+        {
+          nayoks.map(nayok => <li>{nayok}</li>)
+        }
+        
+      </ul>
+      {/* <Cinema nayok="J Bond" naika="Cat W"></Cinema> */}
+      {
+        cinemas.map(cinema =>    <Cinema nayok={cinema.nayok} naika={cinema.naika}></Cinema>)
+      }
+      
     </div>
   );
 }
@@ -20,4 +36,23 @@ function Person(props){
     </div>
   );
 }
+
+function Cinema(props){
+  return (
+    <div className='person'>
+      <h2>Nayok: {props.nayok}</h2>
+      <h4>Nayika: {props.naika}</h4>
+    </div>
+  )
+}
+
+/*
+<Person name="Rubel" naika="Moushomi"></Person>
+<Person name="BappaRaz" naika="Cheka"></Person>
+*/
+
+/*
+<Person name={nayoks[0]} naika="Moushomi"></Person>
+      <Person name={nayoks[1]} naika="Cheka"></Person>
+*/
 export default App;
